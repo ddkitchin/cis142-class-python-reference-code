069b171faa344105af492950adcb8f391906c5b6
ddkitchin <46247238+ddkitchin@users.noreply.github.com>
2022-12-18 12:46:51 -0600

    Intro added

---
+++ Introduction/introductionExamples.py
@@ -0,0 +1,26 @@
+# Hello World @copyright Deborah Kitchin
+print("Hello World")
+
+''' Show a variable in a sentence '''
+myName = 'Debbie'
+print(f"The value for the variable name is a portal for {myName}.")
+ 
+# Add an age to the sentence
+myAge = 57
+print(f"Hello, My name is {myName} and I'm {myAge} years old.")
+
+# blank line
+print()
+
+# Description of Eric Idle 
+firstName = "Eric"
+lastName = "Idle"
+age = 74
+profession = "comedian"
+affiliation = "Monty Python"
+
+# \n will print a blank line after the sentence.
+print(f"Hello, {firstName} {lastName}. You are {age}. You are a {profession}. You were a member of {affiliation}.\n")
+
+# Calculated values
+print(f"{firstName} is {age - myAge} older than {myName}")
