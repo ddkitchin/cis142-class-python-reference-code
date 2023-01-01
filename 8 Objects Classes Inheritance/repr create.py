fe493db916a27fe312685c9cbeb8700821c2b922
ddkitchin <46247238+ddkitchin@users.noreply.github.com>
2023-01-01 17:00:39 -0600

    repr created

---
+++ 8 Objects Classes Inheritance/repr create.py
@@ -0,0 +1,17 @@
+class Weapon:
+    
+    def __init__(self,damage):
+        self._damage = damage
+        
+    def __repr__(self):
+        # user repr to create an object from it.
+        return f'{self.__class__.__name__}({self._damage})'
+    
+def main():
+    w=Weapon(5)
+    print(w)
+    w2=eval(repr(Weapon(10)))
+    print(w2)
+
+if __name__ == "__main__":
+    main()
