8c2ce3675a61c32e4694b8682832681436b3e705
dkitchin <46247238+ddkitchin@users.noreply.github.com>
2023-03-08 09:32:42 -0600

    Update functionProjectPrivacy.py
    allow empty string, only look at first character, and rework prompt for message to match privacy variable

--- 5 Functions/functionProjectPrivacy.py
+++ 5 Functions/functionProjectPrivacy.py
@@ -5,10 +5,12 @@ def main():
     
 #@ return Public or Private based on players preference
 def getPrivacy():
-    privacy=""
-    while privacy  != "Y" and privacy != "N":
-        privacy=input("Do you want your profile protected from others? y or n ").upper()
-    if privacy.upper() == "Y":
+    
+    privacy = ""
+    while (len(privacy) == 0) or (privacy[0].upper() != "Y" and privacy[0].upper() != "N"):
+        privacy = input("Do you want your profile private from other players (Y or N)? ")
+        
+    if privacy[0].upper() == "Y":
         return "Private"
     else:
         return "Public"
