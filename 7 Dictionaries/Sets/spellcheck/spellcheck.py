55d2b04b90375a4c6913e315e590acf85afffd8c
dkitchin <46247238+ddkitchin@users.noreply.github.com>
2023-03-29 13:19:28 -0500

    Add files via upload

--- 7 Dictionaries/Sets/spellcheck/spellcheck.py
+++ 7 Dictionaries/Sets/spellcheck/spellcheck.py
@@ -1,6 +1,6 @@
 ##
-#  This program checks which words.txt in a file are not present in a list of
-#  correctly spelled words.txt.
+#  This program checks which words in a file are not present in a list of
+#  correctly spelled words.
 #
 
 # Import the split function from the regular expression module.
@@ -8,17 +8,17 @@ from re import split
 
 def main() :
    # Read the word list and the document.
-   correctlySpelledWords = readWords("words.txt")
+   correctlySpelledWords = readWords("words")
    documentWords = readWords("alice30.txt")
 
-   # Print all words.txt that are in the document but not the word list.
+   # Print all words that are in the document but not the word list.
    misspellings = documentWords.difference(correctlySpelledWords)
    for word in sorted(misspellings) :
       print(word)
    
-## Reads all words.txt from a file.
+## Reads all words from a file.
 #  @param filename the name of the file
-#  @return a set with all lowercased words.txt in the file. Here, a word is a
+#  @return a set with all lowercased words in the file. Here, a word is a 
 #  sequence of upper- and lowercase letters
 #   
 def readWords(filename) :
@@ -27,7 +27,7 @@ def readWords(filename) :
    
    for line in inputFile :
       line = line.strip()
-      # Use any character other than a-z or A-Z as word delimiters.
+      # Use any character other than a-z or A-Z as word delimiters. The part inside [] is a regular expression.
       parts = split("[^a-zA-Z]+", line)
       for word in parts :
          if len(word) > 0 :
