496099e6c5204ebff4758599237df33f4d61ee42
ddkitchin <46247238+ddkitchin@users.noreply.github.com>
2023-07-20 15:18:12 -0500

    Changed function project

--- 5 Functions/functionProjectPrivacy.py
+++ 5 Functions/functionProjectPrivacy.py
@@ -1,18 +1,31 @@
 #@copyright Deborah Kitchin
+# @copyright Deborah Kitchin
 def main():
     privacy = getPrivacy()
     print(privacy)
-    
-#@ return Public or Private based on players preference
+
+
+def validPrivacy(privacy):
+    # @return True if Y or N were selected otherwise False
+
+    if len(privacy) != 0 and (privacy[0].upper() == "Y" or privacy[0].upper() == "N"):
+        return True
+    else:
+        print("ERROR: Privacy must be Y or N")
+        return False
+
+
 def getPrivacy():
-    
-    privacy = ""
-    while (len(privacy) == 0) or (privacy[0].upper() != "Y" and privacy[0].upper() != "N"):
-        privacy = input("Do you want your profile private from other players (Y or N)? ")
-        
-    if privacy[0].upper() == "Y":
+    # @return validated Public or Private visibility.
+
+    privacy = input("Do you want your profile visible to others Y or N? ")
+    while not validPrivacy(privacy):
+        privacy = input("Do you want your profile visible to others Y or N? ")
+
+    if privacy[0].upper() == "N":
         return "Private"
     else:
         return "Public"
-    
+
+
 main()
--- 5 Functions/functionProjectTemplate.py
+++ 5 Functions/functionProjectTemplate.py
@@ -1,38 +1,125 @@
 #@copyright Deborah
+# @copyright Deborah Kitchin
 def main():
-    name=getName()
-    address=getAddress()
-    playerName=getPlayerName()
-    privacy=getPrivacy()
-    print("Name:", name)
-    print("Address:", address)
-    print("Player Name:",playerName)
-    print("Privacy:",privacy)
-    
+    name = getName()
+    address = getAddress()
+    playerName = getPlayerName()
+    privacy = getPrivacy()
+    print(name)
+    print(address)
+    print(playerName)
+    print(privacy)
+
+
+def validFirstName(first):
+    # @return True if first name is not empty
+
+    if len(first) == 0:
+        print("ERROR: A first name is required")
+        return False
+    else:
+        return True
+
+
+def validMiddleName(middle):
+    # @return middle name and a space unless middle name is empty.
+
+    return "FIX ME"
+
+
+def validLastName(first, last):
+    # @return True is last name is valid otherwise false
+
+    return "FIX ME"
+
+
 def getName():
-    first=""
-    middle=""
-    last=""
-    ''' First name is done for you. You can use this technique for many other 
+    # @return First Middle and Last Name Validated.
+    # There should be a space after the first name.
+
+    ''' First name is done for you. You can use this technique for many other
     parts of this assignment... street, city...'''
-    while len(first) == 0:
-        first=input("Enter first name ")
-    # FIXME: rest of name logic goes in here and invalid setting may need to be moved.
-            
-    return first+" "+middle+" "+last
+    first = input("Enter first name ")
+    while not validFirstName(first):  # Notice the call to validFirstName
+        first = input("Enter first name ")
+
+    middle = "FIX ME"  # use validMiddleName(middle)
+
+    last = "FIX ME"  # use validLastName (first,Last) similar to how first name is done above.
+
+    return first + " " + middle + last
+
+
+def validStreet(street):
+    # @return True if street is not empty
+
+    return "FIX ME"
+
+
+def validCity(city):
+    # @return True if city is not empty
+
+    return "FIX ME"
+
+
+def validState(state):
+    # @return True if state is not empty
+
+    return "FIX ME"
+
+
+def validZipCode(zipCode):
+    # @return True if zipcode is valid
+
+    return "FIX ME"
+
 
 def getAddress():
-    street="FIXME"
-    city="FIXME"
-    state="FIXME"
-    zipCode="FIXME"
-    # FIXME: address logic here
-    return street+", "+city+", "+state+" "+zipCode
+    # @return valid "street, city state zipcode"
+
+    street = "FIX ME"  # use validStreet (street) similar to how first name is done above.
+
+    city = "FIX ME"  # use validCity (city) similar to how first name is done above
+
+    state = "FIX ME"  # use validState (state) similar to how first name is done above
+
+    zipCode = "FIX ME"  # use validZipCode (zipCode) similar to how first name is done above
+
+    return street + ", " + city + ", " + state + " " + zipCode
+
+
+def validPlayerName(playerName):
+    # @return True if valid player name.
+
+    return "FIX ME"
+
+
+def confirmPlayerName(playerName, playerName2):
+    # @return True if player names match, otherwise False
+
+    return "FIX ME"
+
 
 def getPlayerName():
-    return "FIXME: Need to put player name logic in this function"
+    # @return valid player name
+
+    playerName = "FIX ME"  # use validPlayerName (playerName) similar to how first name is done above
+
+    playerName2 = "FIX ME"  # use confirmPlayerName (playerName, playerName2) similar to how first name is done above
+
+    return "FIX ME"
+
+
+def validPrivacy(privacy):
+    # @return True if Y or N were selected otherwise False
+
+    return "FIX ME"
+
 
 def getPrivacy():
-    return "FIXME: Need to put privacy logic in this function"
+    # @return validated Public or Private visibility.
+
+    return "FIX ME"  # use validPrivacy (privacy) similar to how first name is done above
+
 
 main()
