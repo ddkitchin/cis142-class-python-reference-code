054524b0d86256ad161825ee950bec37c358fd61
dkitchin <46247238+ddkitchin@users.noreply.github.com>
2023-09-27 08:13:46 -0500

    Update pickANumberBreak.py

--- 4 Loops/pickANumberBreak.py
+++ 4 Loops/pickANumberBreak.py
@@ -2,7 +2,7 @@ from random import randint
 # Guess the number game. You can play as long as you want
 keepGoing = True
 games = 0  # counts how many games are played
-wins = 0  # counts how many gaems are won
+wins = 0  # counts how many games are won
 while keepGoing:
     games = games + 1
     print("You have 3 tries to guess the number")
