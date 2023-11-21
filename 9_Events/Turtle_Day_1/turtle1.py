e847cd6bd9aed066de800c88d1edbff98984f498
ddkitchin <46247238+ddkitchin@users.noreply.github.com>
2023-11-20 18:09:21 -0600

    showed red in name, hex, and rgb

--- 9_Events/Turtle_Day_1/turtle1.py
+++ 9_Events/Turtle_Day_1/turtle1.py
@@ -6,7 +6,10 @@ turtle.setup(WIN_WIDTH, WIN_HEIGHT)
 win=turtle.Screen()
 
 tim=turtle.Turtle()
-tim.pencolor("red")
+tim.pencolor("red") # red using standard color names.
+#tim.pencolor("#FF0000") # red in hex
+#turtle.colormode(255) # 1) set colormode to 255 to then start using RGB
+#tim.pencolor((255, 0, 0)) # 2) set color red in rgb
 tim.circle(40)
 
 win.mainloop()
