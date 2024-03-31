74b6b09da5758ce1ae81b26061c1ba8213fd483d
ddkitchin <46247238+ddkitchin@users.noreply.github.com>
2024-03-31 12:56:13 -0500

    Added main.

--- 7_Dictionaries/Sets/sets.py
+++ 7_Dictionaries/Sets/sets.py
@@ -1,25 +1,29 @@
 #@copyright Deborah Kitchin
-game102Class=set()
-pythonClass=set()
+def main():
+    game102Class=set()
+    pythonClass=set()
 
-moreStudents=True
-while moreStudents:
-    studentName=input("Enter a student in game 102 ")
-    if studentName != "":
-        game102Class.add(studentName)
-    else:
-        moreStudents=False
+    moreStudents=True
+    while moreStudents:
+        studentName=input("Enter a student in game 102 ")
+        if studentName != "":
+            game102Class.add(studentName)
+        else:
+            moreStudents=False
 
-moreStudents=True
-while moreStudents:
-    studentName=input("Enter a student in python ")
-    if studentName != "":
-        pythonClass.add(studentName)
-    else:
-        moreStudents=False
-    
-print("Game class ",game102Class)
-print("Python class ",pythonClass)
-print("Union ",game102Class.union(pythonClass))
-print("Intersection ",game102Class.intersection(pythonClass))
-print("Difference",game102Class.difference(pythonClass))
+    moreStudents=True
+    while moreStudents:
+        studentName=input("Enter a student in python ")
+        if studentName != "":
+            pythonClass.add(studentName)
+        else:
+            moreStudents=False
+
+    print("Game class ",game102Class)
+    print("Python class ",pythonClass)
+    print("Union ",game102Class.union(pythonClass))
+    print("Intersection ",game102Class.intersection(pythonClass))
+    print("Difference",game102Class.difference(pythonClass))
+
+if __name__ == "__main__":
+    main()
